84ba088e4e273f2338b51222a58776c2d132f7f2
minhkhoinguy <minhkhoinguy@gmail.com>
2022-04-13 15:54:42 -0700

    updated funcion

--- Assets/script.js
+++ Assets/script.js
@@ -2,17 +2,32 @@
 var time = moment().format('LLL');
 $("#currentDay").text(time);
 
+// we want a function to run whenever we go to the page
+// that checks local storage for a value
+// and put's that value in the correct time-block
+
+var description = document.querySelectorAll('.description')
+
+function getSchedule() {
+    description.forEach(function(element) {
+        let val = localStorage.getItem(JSON.stringify(element.id))
+        if(val) {
+            element.textContent = val.replace(/["]+/g, '');
+        }
+    })
+}
+
+getSchedule()
+
 
 //locally stored data
 var saveBtn = document.querySelectorAll('.saveBtn');
 
-let schedule = JSON.parse(localStorage.getItem('schedule')) || [];
 saveBtn.forEach(function (saves) {
     saves.addEventListener('click', function (event) {
         let row = event.target.parentNode
         let myContent = row.querySelector('.description').value;
-        let hour = row.querySelector('.hour').textContent;
-        schedule.push({myContent,hour});
-        localStorage.setItem('schedule', JSON.stringify(schedule));
+        let id = row.querySelector('.description').id;
+        localStorage.setItem(JSON.stringify(id), JSON.stringify(myContent));
     })
 })
